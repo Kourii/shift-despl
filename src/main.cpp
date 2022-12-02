1da32edaef00fd1f4e5c7361d2beae6dd3070fe5
Kourii <79379864+Kourii@users.noreply.github.com>
2022-12-02 14:47:55 -0300

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
-}
 
-void loop() {
-  // put your main code here, to run repeatedly:
+uint8_t output = 0b10101010;
+uint8_t i = 0;
+uint8_t nextBit;
+
+int main(){
+
+  while (1){
+
+    PORTD &= ~(1<<PD4);
+    for (i = 0; i < 8; i++)
+    {
+      nextBit=(output>>1)&0b00000001;
+      if(nextBit){
+        PORTD|=(1<<PD5);//Salida
+      }
+
+
+    }
+    
+  }
 }
